04d76a976020f082d4a3d8030252f9ec421cc6f6
chriskjaer <chriskjaer.dk@gmail.com>
2016-10-11 23:00:25 +0200

    chore: added analytics

--- html.js
+++ html.js
@@ -14,8 +14,18 @@ module.exports = React.createClass({
   render () {
     const head = Helmet.rewind()
 
+    let analytics
     let css
     if (process.env.NODE_ENV === 'production') {
+      analytics = <script dangerouslySetInnerHTML={{ __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-85526169-1', 'auto');
+        ga('send', 'pageview');
+      `}} />
       css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
     }
 
@@ -34,6 +44,7 @@ module.exports = React.createClass({
         </head>
         <body>
           <div id='react-mount' dangerouslySetInnerHTML={{ __html: this.props.body }} />
+          {analytics}
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
         </body>
       </html>
